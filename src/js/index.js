6a69ce0da15b9ee193ddbdfc57421ed51d69a6e6
freeivanzw <freeivanzw@gmail.com>
2022-10-26 17:58:21 +0300

    start task list

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,22 @@
+var $black = $('.shadow');
+$black.on('click', function () {
+    $(this).addClass('hidden');
+})
+
 $('.open-site_panel').on('click', function (e) {
     e.preventDefault();
 
     $(this).toggleClass('btn_contrast');
     $('.site_panel').toggleClass('active');
+})
+
+$('.notification_btn').on('click', function (e) {
+    e.preventDefault();
+
+    $black.toggleClass('hidden');
+    $('.notification_list').toggleClass('active');
+
+    $black.on('click', function () {
+        $('.notification_list').removeClass('active');
+    })
 })
