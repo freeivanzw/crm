8bf4f515e16e550a9ced2590f11ab23e07c1ee46
freeivanzw <freeivanzw@gmail.com>
2022-10-27 17:06:22 +0300

    start list page

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,63 @@
-var $black = $('.shadow');
-$black.on('click', function () {
-    $(this).addClass('hidden');
-})
-
-$('.open-site_panel').on('click', function (e) {
-    e.preventDefault();
-
-    $(this).toggleClass('btn_contrast');
-    $('.site_panel').toggleClass('active');
-})
-
-$('.notification_btn').on('click', function (e) {
-    e.preventDefault();
-
-    $black.toggleClass('hidden');
-    $('.notification_list').toggleClass('active');
-
+$(function () {
+    var $black = $('.shadow');
     $black.on('click', function () {
-        $('.notification_list').removeClass('active');
+        $(this).addClass('hidden');
     })
+
+    $('.open-site_panel').on('click', function (e) {
+        e.preventDefault();
+
+        $(this).toggleClass('btn_contrast');
+        $('.site_panel').toggleClass('active');
+    })
+
+    $('.notification_btn').on('click', function (e) {
+        e.preventDefault();
+
+        $black.toggleClass('hidden');
+        $('.notification_list').toggleClass('active');
+
+        $black.on('click', function () {
+            $('.notification_list').removeClass('active');
+        })
+    })
+
+    var spark4 = {
+        chart: {
+            id: 'sparkline4',
+            type: 'line',
+            height: 100,
+            sparkline: {
+                enabled: true
+            },
+            group: 'sparklines'
+        },
+        series: [{
+            name: 'teal',
+            data: [15, 75, 47, 65, 14, 32, 19, 54, 44, 61]
+        }],
+        stroke: {
+            curve: 'smooth'
+        },
+        markers: {
+            size: 0
+        },
+        tooltip: {
+            fixed: {
+                enabled: true,
+                position: 'right'
+            },
+            x: {
+                show: false
+            }
+        },
+        colors: ['#ffffff'],
+        xaxis: {
+            crosshairs: {
+                width: 1
+            },
+        }
+    }
+
+    new ApexCharts(document.querySelector("#spark4"), spark4).render();
 })
